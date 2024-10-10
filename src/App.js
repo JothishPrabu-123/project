6722a048881c48ca4e7cb2360849c799f5f2d46e
JothishPrabu-123 <139376877+JothishPrabu-123@users.noreply.github.com>
2024-10-10 12:24:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrafficFlow from './components/TrafficFlow';
+
+function App() {
+  return (
+    <div className="App">
+      <TrafficFlow />
+    </div>
+  );
+}
+
+export default App;
